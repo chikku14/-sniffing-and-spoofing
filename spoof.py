c9f8111e0dd5de07cc07c801f88662fc07bb4e08
unknown14 <163493530+chikku14@users.noreply.github.com>
2026-04-24 15:29:49 +0530

    Create spoof.py

---
+++ spoof.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from scapy.all import IP, ICMP, send
+
+packet = IP(dst="8.8.8.8") / ICMP()
+
+send(packet, count=4)
+print("Spoofed packets sent.")
+
+
+
+requirements.txt
+scapy
